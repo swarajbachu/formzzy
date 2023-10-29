0944ceab7f42f658f895bdbde727a0f466cba7d5
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-29 20:04:21 +0530

    form synced

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -10,9 +10,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import React from "react";
-import {  useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   name: z.string().min(2, {
@@ -21,21 +25,45 @@ const formSchema = z.object({
   email: z.string().email({
     message: "invalid email address.",
   }),
-  
+  days: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "You have to select at least one item.",
+  }),
+  food: z.enum(["None", "Vegetarian", "Vegan"], {
+    required_error: "You need to select a notification type.",
+  }),
 });
+
+const items = [
+  {
+    id: "Day+1",
+    label: "Day 1",
+  },
+  {
+    id: "Day+2",
+    label: "Day 2",
+  },
+  {
+    id: "Day+3",
+    label: "Day 3",
+  },
+] as const;
 
 function MyForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
+      email: "",
+      days: [],
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log("data")
+    console.log(data);
+    toast({
+      title: "Hehe! Form submitted successfully 🎉",
+    });
   }
 
   return (
@@ -45,14 +73,113 @@ function MyForm() {
           control={form.control}
           name="name"
           render={({ field }) => (
-            <FormItem >
+            <FormItem>
               <FormLabel>Name</FormLabel>
               <FormControl>
                 <Input className="w-[600px]" placeholder="shadcn" {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input className="w-[600px]" placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="days"
+          render={() => (
+            <FormItem>
+              <div className="mb-4">
+                <FormLabel className="text-base">
+                  What days will you attend?
+                </FormLabel>
+              </div>
+              {items.map((item) => (
+                <FormField
+                  key={item.id}
+                  control={form.control}
+                  name="days"
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={item.id}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(item.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, item.id])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== item.id
+                                    )
+                                  );
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="text-sm font-normal">
+                          {item.label}
+                        </FormLabel>
+                      </FormItem>
+                    );
+                  }}
+                />
+              ))}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="food"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel className="text-base">Dietary restrictions</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="None" />
+                    </FormControl>
+                    <FormLabel className="font-normal">None</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="Vegetarian" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Vegetarian</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="Vegan" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Vegan</FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+              <Button type="submit">Submit</Button>
+
       </form>
     </Form>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from 'next/image'
-import MyForm from './form'
+import Image from "next/image";
+import MyForm from "./form";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MyForm />
+      <Card className="flex flex-col items-center justify-center space-y-8 p-12">
+        <h1 className="text-xl font-bold">Event registration</h1>
+        <MyForm />
+      </Card>
     </main>
-  )
+  );
 }
